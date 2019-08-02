882b18493cc6cdc030b65bb104b3b5613a3a3c52
outtaway <konst8403@gmail.com>
2019-08-02 19:43:22 +0300

    added sdl2

--- client/graphicLibraries/sfmldll/sfml.cpp
+++ client/graphicLibraries/sfmldll/sfml.cpp
@@ -1,10 +1,12 @@
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
+#include <iostream>
 #include "../../key.h"
 
 namespace
 {
     static sf::RenderWindow *window;
+
 
     static uint8_t *gridPtr;
     static uint8_t gridWidth;
@@ -28,59 +30,67 @@ extern "C"
 void    destroy()
 {
     window->close();
-    delete window;
+s    delete window;
 }
 
 extern "C"
 void    draw()
 {
-    sf::Event   event;
+	std::cout << "isOpen" << window->isOpen() << std::endl;
+	if (window->isOpen())
+	{
+		sf::Event event;
 
-    while (window->pollEvent(event))
-        if (event.type == sf::Event::EventType::Closed)
-            exit(EXIT_SUCCESS);
+		while (window->pollEvent(event))
+			if (event.type == sf::Event::EventType::Closed)
+				exit(EXIT_SUCCESS);
 
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
-        exit(EXIT_SUCCESS);
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+		{
+			window->close();
+			return;
+		}
 
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
-        window->close();
+//    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
 
-    if (window->isOpen())
-    {
-        window->clear();
+		if (window->isOpen())
+		{
+			window->clear();
 
-        for (size_t y = 0; y < gridHeight; y++)
-            for (size_t x = 0; x < gridWidth; x++)
-            {
-                sf::RectangleShape  rect(sf::Vector2f(::squareSide, ::squareSide));
-                rect.setPosition(sf::Vector2f(::squareSide * x, ::squareSide * y));
-                if (!::gridPtr[y * gridWidth + x])
-                    rect.setFillColor(sf::Color::Black);
-                else if (gridPtr[y * gridWidth + x] == 1)
-                    rect.setFillColor(sf::Color::Green);
-                else
-                    rect.setFillColor(sf::Color::Red);
-                window->draw(rect);
-            }
+			for (size_t y = 0; y < gridHeight; y++)
+				for (size_t x = 0; x < gridWidth; x++)
+				{
+					sf::RectangleShape rect(
+						sf::Vector2f(::squareSide, ::squareSide));
+					rect.setPosition(
+						sf::Vector2f(::squareSide * x, ::squareSide * y));
+					if (!::gridPtr[y * gridWidth + x])
+						rect.setFillColor(sf::Color::Black);
+					else if (gridPtr[y * gridWidth + x] == 1)
+						rect.setFillColor(sf::Color::Green);
+					else
+						rect.setFillColor(sf::Color::Red);
+					window->draw(rect);
+				}
 
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
-            lastPressed = key::LEFT;
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
-            lastPressed = key::RIGHT;
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
-            lastPressed = key::DOWN;
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
-            lastPressed = key::UP;
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Num1))
-            lastPressed = key::NB1;
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Num2))
-            lastPressed = key::NB2;
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Num3))
-            lastPressed = key::NB3;
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+				lastPressed = key::LEFT;
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+				lastPressed = key::RIGHT;
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+				lastPressed = key::DOWN;
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+				lastPressed = key::UP;
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Num1))
+				lastPressed = key::NB1;
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Num2))
+				lastPressed = key::NB2;
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Num3))
+				lastPressed = key::NB3;
 
-        window->display();
-    }
+			window->display();
+		}
+	}
 }
 
 extern "C"
