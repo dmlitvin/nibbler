e60953e8d10c949686fcbf900621e526cea4aef3
Dmitro LITVINENKO <dmlitvin@e1r6p16.unit.ua>
2019-08-03 11:23:03 +0300

    buildScriptWorks

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+cmake -S server -B server
+cmake -S client -B client
+cmake -S client/graphicLibraries/ncurses -B client/graphicLibraries/ncurses
+cmake -S client/graphicLibraries/sfml -B client/graphicLibraries/sfml
+cmake -S client/graphicLibraries/sdl2 -B client/graphicLibraries/sdl2
+make -C server -j4
+make -C client -j4
+make -C client/graphicLibraries/ncurses -j4
+make -C client/graphicLibraries/sfml -j4
+make -C client/graphicLibraries/sdl2 -j4
